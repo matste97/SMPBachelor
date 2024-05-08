6bccacff436ccc03d4587323909701a0c30d0f1e
Kim <71408597+Kim3n@users.noreply.github.com>
2024-05-08 16:47:34 +0200

    Changed dummycontroller to facebook controller to serve facebook page

---
+++ SMPBachlelorApp/src/main/java/no/ntnu/SMPBachelor/controllers/FacebookAPIController.java
@@ -0,0 +1,18 @@
+package no.ntnu.SMPBachelor.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FacebookAPIController {
+    @GetMapping("facebook")
+    @Operation(
+            summary = "Show facebook page",
+            description = "Displays the facebook page"
+    )
+    public String facebook(Model model) {
+        return "facebook";
+    }
+}
